9a413800038a35c1131d81371c9097e1c02609b1
zanzandonadii <zanzandonadi@gmail.com>
2025-10-30 00:02:22 -0300

    fiz a tela adm nao funcional

---
+++ html/admin.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/admin.css">
+    <title>Clarify</title>
+    <link rel="shortcut icon" type="imagex/png" href="/images/clarifyv1.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body style="background-color: #8AB3D0;">
+    <h1>Bem-vindo, administrador!</h1>
+    <div class="controle">
+        <div class="titulo">
+            <h3>Solicitações para a criação de um perfil de professores</h3>
+        </div>
+        <div class="table-responsive-xxl">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Usuário</th>
+                        <th scope="col">Arquivo</th>
+                        <th scope="col">Ação</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    <tr>
+                        <td>Alex</td>
+                        <td>zanzandonadi</td>
+                        <td>*arquivo do diploma</td>
+                        <td>Aceitar | Excluir</td>
+                    </tr>
+                    <tr>
+                        <td>Vithor</td>
+                        <td>freakofsorrow</td>
+                        <td>*arquivo do diploma</td>
+                        <td>Aceitar | Excluir</td>
+                    </tr>
+                    <tr>
+                        <td>Isabelle</td>
+                        <td>kvra</td>
+                        <td>*arquivo do diploma</td>
+                        <td>Aceitar | Excluir</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
